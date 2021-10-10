68e89c2fc06b2fdb73b447246e2eb791f230189c
mariohirzi <MarioHirzi@gmail.com>
2021-10-10 21:30:24 +0200

    main

--- Sudoku_with_Nunit/NUnitTest/UnitTest1.cs
+++ Sudoku_with_Nunit/NUnitTest/UnitTest1.cs
@@ -35,6 +35,44 @@ namespace NUnitTest
 
         }
 
+        [Test]
+        public void Test2()
+        {
+            string su = "410065007" +
+                        "006007480" +
+                        "207490006" +
+                        "060070100" +
+                        "301500072" +
+                        "090042308" +
+                        "108600029" +
+                        "020018640" +
+                        "600300010";
+
+            string correctSu = "413865297" +
+                        "956237481" +
+                        "287491536" +
+                        "862973154" +
+                        "341586972" +
+                        "795142368" +
+                        "138654729" +
+                        "529718643" +
+                        "674329815";
+
+            SudokuAbstract result = sudoku.Solve(su);
+
+            string resultInString = ConvertSudokuToString(result);
+
+            if (correctSu == resultInString)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+        }
+
         public string ConvertSudokuToString(SudokuAbstract temp)
         {
             string result = string.Empty;
